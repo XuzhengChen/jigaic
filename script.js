66a09126b7203ef9d09e8a9bd54e67c4ae18aef2
XuzhengChen <2000017764@stu.pku.edu.cn>
2025-02-01 17:45:40 +0800

    a test on 202502011745

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ document.querySelector('#course-info').addEventListener('click', function () {
     alert('这里是课程信息！');
 });
 */
-// 获取按钮元素
-const pdfButton = document.getElementById('pdf-button');
-
-// 添加点击事件
-pdfButton.addEventListener('click', function () {
-    //替换为你的 PDF 文件链接
-    window.location.href = 'https://github.com/XuzhengChen/jigaic/blob/main/references/sol_to_oj/sol1.pdf';
-});
